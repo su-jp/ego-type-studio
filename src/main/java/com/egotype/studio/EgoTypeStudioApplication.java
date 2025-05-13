7a5546f6338854c6a59e8a3089da512e90492ad0
Sujin <su_jp@naver.com>
2025-05-13 16:28:15 +0900

    Initial commit

---
+++ src/main/java/com/egotype/studio/EgoTypeStudioApplication.java
@@ -0,0 +1,13 @@
+package com.egotype.studio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgoTypeStudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgoTypeStudioApplication.class, args);
+	}
+
+}
