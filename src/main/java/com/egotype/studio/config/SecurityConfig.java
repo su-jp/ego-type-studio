95b4c9d6bcf350d50b7b8098ee06d52adf533379
Sujin <su_jp@naver.com>
2025-05-16 18:59:58 +0900

    chore: Security 설정에 경로 허용 추가
    chore: Security 설정에 경로 허용 추가
    
    - Security 설정에 /signup, /login 경로 허용 추가

--- src/main/java/com/egotype/studio/config/SecurityConfig.java
+++ src/main/java/com/egotype/studio/config/SecurityConfig.java
@@ -14,7 +14,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/", "/css/**", "/js/**", "/images/**").permitAll()
+                        .requestMatchers("/", "/signup", "/login", "/css/**", "/js/**", "/images/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
