2cdcd6717e9c817b18423cf9d51165af19f0a8c8
Sujin <su_jp@naver.com>
2025-05-16 19:01:44 +0900

    feat: 회원가입 화면 1차 퍼블리싱 및 /signup 뷰 연동 완료
    feat: 회원가입 화면 1차 퍼블리싱 및 /signup 뷰 연동 완료
    
    - signup.html 추가 및 일부 js 기능 구현
    - ViewController에서 /signup 경로 연결

--- src/main/java/com/egotype/studio/controller/ViewController.java
+++ src/main/java/com/egotype/studio/controller/ViewController.java
@@ -10,4 +10,9 @@ public class ViewController {
     public String introPage() {
         return "intro";
     }
+
+    @GetMapping("/signup")
+    public String signupPage() {
+        return "signup";
+    }
 }
